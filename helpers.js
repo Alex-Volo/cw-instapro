6a66790d8e2efb1ff7ea9d129c12c3b98c1c78fe
Alex Volo <alex_volo@bk.ru>
2023-04-16 20:46:00 +0900

    Registration and login validation

--- helpers.js
+++ helpers.js
@@ -14,16 +14,31 @@ export function removeUserFromLocalStorage(user) {
   window.localStorage.removeItem("user");
 }
 
-export function validate(input) {
+export function validate(input, placeholder = '') {
   if (input.value === '' || input.value === '\n') {
       input.classList.add('error');
       input.placeholder = 'Поле не может быть пустым!';
       input.value = '';
       setTimeout(() => {
           input.classList.remove('error')
-          input.placeholder = ``;
+          input.placeholder = placeholder;
       }, 1500);
   } else {
       return true;
+  }
+}
+
+export function checkLogin(input, placeholder = ''){
+  const pattern = /^[a-zа-я][a-zа-я0-9-_]*$/i;
+  if(!pattern.test(input.value)){
+    input.classList.add('error');
+    input.placeholder = 'Поле не может содержать специальные символы, кроме "-" и "_"';
+    input.value = '';
+    setTimeout(() => {
+        input.classList.remove('error')
+        input.placeholder = placeholder;
+    }, 1500);
+  } else {
+    return true;
   }
 }
